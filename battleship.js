e81a5dc9e9cae1c4057672c664b4eeaf7e44f803
aperrin <amandine.perrin@pasteur.fr>
2018-12-04 19:54:22 +0100

    check availability of position before adding a ship

--- battleship.js
+++ battleship.js
@@ -12,14 +12,44 @@ class Board {
         }
     }
 
-  // add a new ship on the grid
+    // add a new ship on the grid
+    // returns true if ship added, false otherwise (problems with grid/size)
     add_ship(ship, row, col){
-        let x_offset = 0, y_offset = 0
+        // Verify if ship position available (no ship and inside of grid)
+        if (!this.is_free(ship, row, col))
+            return false;
+
+        // Add the ship in the grid
+        let x_offset = 0, y_offset = 0;
         ship.orientation == "V" ? x_offset = 1 : y_offset = 1;
 
-        for (let tile=0 ; tile<ship.size ; tile++) {
+        for (let tile=0 ; tile<ship.size ; tile++)
             this.grid[row+tile*x_offset][col+tile*y_offset] = ship;
-        }
+
+        return true;
+    }
+
+
+    is_free(ship, row, col){
+        let x_offset = 0, y_offset = 0;
+        ship.orientation == "V" ? x_offset=1 : y_offset=1;
+
+        // Verify if first and last positions of ship are inside the grid
+        if (row<0 || row >= this.size || col<0 || col >= this.size)
+            return false;
+
+        let last_row = row + x_offset*(ship.size - 1);
+        let last_col = col + y_offset*(ship.size - 1);
+        if (last_row<0 || last_row >= this.size || last_col<0 
+            || last_col>=this.size)
+            return false;
+
+        // Add the ship in the grid
+        for (let tile=0 ; tile<ship.size ; tile++)
+            if (this.grid[row+tile*x_offset][col+tile*y_offset] != null)
+                return false;
+
+        return true;
     }
 
   /* Other player fired on the given tile
@@ -52,7 +82,7 @@ class Ship{
         this.orientation = orientation;
         this.states = [];
         for (let i=0 ; i<size ; i++)
-            this.states.push(i); 
+            this.states.push(0); 
     }
 
     rotate(){
@@ -65,8 +95,9 @@ class Ship{
 
 
 var s1 = new Ship("B", 2, "V");
-var s2 = new Ship("A", 3);
+var s2 = new Ship("A", 3, "H");
 var b = new Board(4);
+// s2.rotate();  
 b.add_ship(s1, 0, 0);
-// b.add_ship(s2)
+b.add_ship(s2, 3, 0)
 b.print_grid();
